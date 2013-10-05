c1a293df34f74e30cd987f33ff199631cf5488e0
Raphaël MARQUES <work.rmarques@gmail.com>
2013-10-06 01:06:48 +0200

    EPSI Courses Custom + Diap Improv.

--- js/diap.js
+++ js/diap.js
@@ -13,71 +13,24 @@
 
 function getPresentSlide() {
 	var x = document.querySelectorAll(".reveal .slides section.present:not(.past):not(.future):not(.stack)");
-	for(var i = 0, len = x.length; i < len; ++i) {
-		if(x[i].classList.length === 1) {
+	for(var i = 0, len = x.length; i < len; ++i)
+		if(x[i].classList.length === 1)
 			return x[i];
-		}
-	}
 	return null;
 }
 
 function isDataSetOn(obj, data_marker) {
-    for (var i = 0, len = obj.attributes.length; i < len; ++i)
-        if (obj.attributes[i].name === data_marker)
-            return true;
-    return false;
+    return obj.getAttribute(data_marker) !== null;
 }
 
-function getValueOfData(obj, data_marker) {
-    return obj.getAttribute(data_marker);
-}
-
-function getParentByTagOf(obj, parentTag) {
-    var p = obj.parentNode;
-    while (p.tagName !== parentTag)
-        p = p.parentNode;
-    return p;
-}
-
-function getNumberOfChildsByTag(parent, childTag) {
-    var n = 0;
-    for (var i = 0, len = parent.childNodes.length; i < len; ++i) {
-        if (parent.childNodes[i].tagName === childTag)
-        ++n;
-    }
-    return n;
-}
-
-function getScreenSize(wh) {
-    var w = window,
-        d = document,
-        e = d.documentElement,
-        g = d.getElementsByTagName("body")[0],
-        x = w.innerWidth || e.clientWidth || g.clientWidth,
-        y = w.innerHeight || e.clientHeight || g.clientHeight;
-    return (wh === "width" ? x : y);
-}
-
-function getStyleElement(element, style_element) {
+function getStyleElement(element, style_element, pre_computed) {
+    if(!pre_computed)
+        pre_computed = false;
     if (style_element !== "" && element !== "") {
-        var x = document.querySelector(element);
-        var r = new RegExp("[;]+", "g");
-
-        if (x.getAttribute("style")) {
-            var t = x.getAttribute("style").split(r);
-
-            var v, r2 = new RegExp("[:]+", "g");
-            for (var i = 0, len = t.length; i < len; ++i) {
-                v = "";
-                t[i] = t[i].replace(" ", "");
-                for (var j = 0, lan = t[i].length; j < lan; ++j) {
-                    v += t[i][j];
-                    if (v === style_element)
-                        return t[i].split(r2)[1];
-                }
-            }
-        }
-        return "";
+        var e = pre_computed ? element : document.querySelector(element);
+        var s = (e === null ? null : document.defaultView.getComputedStyle(e));
+        var r = (s === null ? null : s.getPropertyValue(style_element));
+        return r === null ? "" : r;
     }
     return "";
 }
@@ -94,11 +47,11 @@ function initStyleFormatting() {
 function applyStyle(data_marker, CSSProperty, defaultValue) {
     if (!defaultValue)
         defaultValue = "";
-    var t = document.querySelectorAll("[" + data_marker + "]"),
-        x;
+    var t = document.querySelectorAll("[" + data_marker + "]");
+    var x;
     for (var i = 0, len = t.length; i < len; ++i) {
-        x = getValueOfData(t[i], data_marker);
-        t[i].style.cssText = CSSProperty + ":" + (x === "" ? defaultValue : x) + ";" + t[i].style.cssText;
+        x = t[i].getAttribute(data_marker);
+        t[i].setAttribute("style", CSSProperty + ":" + (x === "" ? defaultValue : x) + ";" + t[i].style.cssText);
         t[i].removeAttribute(data_marker);
     }
 }
@@ -115,10 +68,9 @@ function initImages() {
         y.setAttribute("style", "width:" + x[i].width + "px;height:" + perfectShape(x[i].height) + "px;margin: auto auto 0 auto;");
         var p = x[i].parentNode;
 
-        if (!isDataSetOn(x[i], "data-no-bottom-shadow")) {
+        if (!isDataSetOn(x[i], "data-no-bottom-shadow"))
             y.setAttribute("class", "imgShadow");
-
-        } else x[i].removeAttribute("data-no-bottom-shadow");
+        else x[i].removeAttribute("data-no-bottom-shadow");
 
         if (!isDataSetOn(x[i], "data-no-title-caption") && x[i].title !== "") {
             p.removeChild(x[i]);
@@ -126,7 +78,7 @@ function initImages() {
             var q = document.createElement("div");
             q.setAttribute("class", "img_caption");
             q.innerText = x[i].title;
-            q.style.cssText = "width:" + s + "px;margin-left:" + (x[i].width - 10 - s) + "px;";
+            q.setAttribute("style", "width:" + s + "px;margin-left:" + (x[i].width - 10 - s) + "px;");
             y.appendChild(q);
         } else {
             p.removeChild(x[i]);
@@ -147,11 +99,14 @@ function perfectShape(x) {
  *********************************************/
 
 function initSlideNumber() {
+    var dv = document.createElement("div");
+
     var tl = document.createElement("p");
     var tr = document.createElement("p");
     var bl = document.createElement("p");
     var br = document.createElement("p");
 
+    dv.setAttribute("style", "height:25px;");
     tl.setAttribute("class", "slide_number_tl");
     tr.setAttribute("class", "slide_number_tr");
     bl.setAttribute("class", "slide_number_bl");
@@ -162,6 +117,7 @@ function initSlideNumber() {
     bl.innerHTML = "<span class='currentSlide'></span>" + getTotalSlide();
     br.innerHTML = "<span class='currentSlide'></span>" + getTotalSlide();
 
+    document.querySelector(".reveal").appendChild(dv);
     document.querySelector(".reveal").appendChild(tl);
     document.querySelector(".reveal").appendChild(tr);
     document.querySelector(".reveal").appendChild(br);
@@ -191,16 +147,16 @@ function slideNumber(event) {
 function hideCounter(counter) {
     switch (counter) {
     case "top-right":
-        document.querySelector(".slide_number_tr").style.cssText = "visibility:hidden;-webkit-animation:hideSlideNumberTR .5s;-moz-animation:hideSlideNumberTR .5s;-ms-animation:hideSlideNumberTR .5s;-o-animation:hideSlideNumberTR .5s;animation:hideSlideNumberTR .5s;";
+        document.querySelector(".slide_number_tr").setAttribute("style", "visibility:hidden;-webkit-animation:hideSlideNumberTR .5s;-moz-animation:hideSlideNumberTR .5s;-ms-animation:hideSlideNumberTR .5s;-o-animation:hideSlideNumberTR .5s;animation:hideSlideNumberTR .5s;");
         break;
     case "top-left":
-        document.querySelector(".slide_number_tl").style.cssText = "visibility:hidden;-webkit-animation:hideSlideNumberTL .5s;-moz-animation:hideSlideNumberTL .5s;-ms-animation:hideSlideNumberTL .5s;-o-animation:hideSlideNumberTL .5s;animation:hideSlideNumberTL .5s;";
+        document.querySelector(".slide_number_tl").setAttribute("style", "visibility:hidden;-webkit-animation:hideSlideNumberTL .5s;-moz-animation:hideSlideNumberTL .5s;-ms-animation:hideSlideNumberTL .5s;-o-animation:hideSlideNumberTL .5s;animation:hideSlideNumberTL .5s;");
         break;
     case "bottom-right":
-        document.querySelector(".slide_number_br").style.cssText = "visibility:hidden;-webkit-animation:hideSlideNumberTR .5s;-moz-animation:hideSlideNumberTR .5s;-ms-animation:hideSlideNumberTR .5s;-o-animation:hideSlideNumberTR .5s;animation:hideSlideNumberTR .5s;";
+        document.querySelector(".slide_number_br").setAttribute("style", "visibility:hidden;-webkit-animation:hideSlideNumberTR .5s;-moz-animation:hideSlideNumberTR .5s;-ms-animation:hideSlideNumberTR .5s;-o-animation:hideSlideNumberTR .5s;animation:hideSlideNumberTR .5s;");
         break;
     case "bottom-left":
-        document.querySelector(".slide_number_bl").style.cssText = "visibility:hidden;-webkit-animation:hideSlideNumberTL .5s;-moz-animation:hideSlideNumberTL .5s;-ms-animation:hideSlideNumberTL .5s;-o-animation:hideSlideNumberTL .5s;animation:hideSlideNumberTL .5s;";
+        document.querySelector(".slide_number_bl").setAttribute("style", "visibility:hidden;-webkit-animation:hideSlideNumberTL .5s;-moz-animation:hideSlideNumberTL .5s;-ms-animation:hideSlideNumberTL .5s;-o-animation:hideSlideNumberTL .5s;animation:hideSlideNumberTL .5s;");
         break;
     }
 }
@@ -220,14 +176,10 @@ function getVisibleCounter() {
 function displaySlideNumber(event) {
     var n = slideNumber(event);
     var v = getVisibleCounter();
+
     if (n !== "") {
         var s = document.querySelectorAll(".currentSlide");
-        var x = getValueOfData(getPresentSlide(), "data-counter-position");
-
-
-        //console.log("actuel = " + x);
-        //console.log("ancien = " + v);
-
+        var x = getPresentSlide().getAttribute("data-counter-position");
 
         for (var i = 0, len = s.length; i < len; ++i)
             s[i].innerText = n;
@@ -236,17 +188,17 @@ function displaySlideNumber(event) {
             hideCounter(v);
             switch (x) {
             case "top-right":
-                document.querySelector(".slide_number_tr").style.cssText = "visibility:visible;-webkit-animation:displaySlideNumberTR .5s;-moz-animation:displaySlideNumberTR .5s;-ms-animation:displaySlideNumberTR .5s;-o-animation:displaySlideNumberTR .5s;animation:displaySlideNumberTR .5s;";
+                document.querySelector(".slide_number_tr").setAttribute("style", "visibility:visible;-webkit-animation:displaySlideNumberTR .5s;-moz-animation:displaySlideNumberTR .5s;-ms-animation:displaySlideNumberTR .5s;-o-animation:displaySlideNumberTR .5s;animation:displaySlideNumberTR .5s;");
                 break;
             case "bottom-left":
-                document.querySelector(".slide_number_bl").style.cssText = "visibility:visible;-webkit-animation:displaySlideNumberTL .5s;-moz-animation:displaySlideNumberTL .5s;-ms-animation:displaySlideNumberTL .5s;-o-animation:displaySlideNumberTL .5s;animation:displaySlideNumberTL .5s;";
+                document.querySelector(".slide_number_bl").setAttribute("style", "visibility:visible;-webkit-animation:displaySlideNumberTL .5s;-moz-animation:displaySlideNumberTL .5s;-ms-animation:displaySlideNumberTL .5s;-o-animation:displaySlideNumberTL .5s;animation:displaySlideNumberTL .5s;");
                 break;
             case "bottom-right":
-                document.querySelector(".slide_number_br").style.cssText = "visibility:visible;-webkit-animation:displaySlideNumberTR .5s;-moz-animation:displaySlideNumberTR .5s;-ms-animation:displaySlideNumberTR .5s;-o-animation:displaySlideNumberTR .5s;animation:displaySlideNumberTR .5s;";
+                document.querySelector(".slide_number_br").setAttribute("style", "visibility:visible;-webkit-animation:displaySlideNumberTR .5s;-moz-animation:displaySlideNumberTR .5s;-ms-animation:displaySlideNumberTR .5s;-o-animation:displaySlideNumberTR .5s;animation:displaySlideNumberTR .5s;");
                 break;
             case "top-left":
             default:
-                document.querySelector(".slide_number_tl").style.cssText = "visibility:visible;-webkit-animation:displaySlideNumberTL .5s;-moz-animation:displaySlideNumberTL .5s;-ms-animation:displaySlideNumberTL .5s;-o-animation:displaySlideNumberTL .5s;animation:displaySlideNumberTL .5s;";
+                document.querySelector(".slide_number_tl").setAttribute("style", "visibility:visible;-webkit-animation:displaySlideNumberTL .5s;-moz-animation:displaySlideNumberTL .5s;-ms-animation:displaySlideNumberTL .5s;-o-animation:displaySlideNumberTL .5s;animation:displaySlideNumberTL .5s;");
                 break;
             }
         }
@@ -260,24 +212,32 @@ function displaySlideNumber(event) {
 function initCodes() {
     var x = document.querySelectorAll(".reveal .slides pre");
 
-    var currentParent, node; //, childs;
+    var currentParent, node;
     for (var i = 0, len = x.length; i < len; ++i) {
-        x[i].style.cssText = (isDataSetOn(x[i], "data-no-shadow") ? "" : "-webkit-box-shadow: 0px 0px 100px 10px rgba(0, 0, 0, 0.3);-moz-box-shadow: 0px 0px 100px 10px rgba(0, 0, 0, 0.3);-ms-box-shadow: 0px 0px 100px 10px rgba(0, 0, 0, 0.3);-o-box-shadow: 0px 0px 100px 10px rgba(0, 0, 0, 0.3);box-shadow: 0px 0px 100px 10px rgba(0, 0, 0, 0.3);") + x[i].style.cssText;
+        x[i].setAttribute("style", (isDataSetOn(x[i], "data-no-shadow") ? "" : "-webkit-box-shadow: 0px 0px 100px 10px rgba(0, 0, 0, 0.3);-moz-box-shadow: 0px 0px 100px 10px rgba(0, 0, 0, 0.3);-ms-box-shadow: 0px 0px 100px 10px rgba(0, 0, 0, 0.3);-o-box-shadow: 0px 0px 100px 10px rgba(0, 0, 0, 0.3);box-shadow: 0px 0px 100px 10px rgba(0, 0, 0, 0.3);") + x[i].style.cssText);
         if (isDataSetOn(x[i], "data-caption")) {
             if (currentParent !== x[i].parentNode) {
                 currentParent = x[i].parentNode;
                 node = 0;
-                //childs = getNumberOfChildsByTag(x[i].parentNode, "PRE");
             } else ++node;
 
-            var s = getValueOfData(x[i], "data-caption").length * 8.2;
+            var s = x[i].getAttribute("data-caption").length * 8.2;
             var y = document.createElement("div");
+            y.innerText = x[i].getAttribute("data-caption");
             y.setAttribute("class", "code_caption");
-            y.innerText = getValueOfData(x[i], "data-caption");
-            y.style.cssText = "width:" + s + "px;margin-left:" + (((getStyleElement(".slides", "width").replace("px", "") * 95) / 100) - s - 20) + "px;margin-top:16px;"; //" + (childs > 1 ? (node === 0 ? "6" : "1") : "16") + "px;";
+
             x[i].parentNode.insertBefore(y, x[i]);
             x[i].parentNode.insertBefore(document.createElement("br"), y);
             x[i].removeAttribute("data-caption");
+
+            var m = getStyleElement(".slides", "width").replace("px", "");
+            var w = getStyleElement(x[i], "width", true).replace("px", "");
+            var p = getStyleElement(y, "border-left-width", true).replace("px", "") * 1 +
+                    getStyleElement(y, "border-right-width", true).replace("px", "") * 1 +
+                    getStyleElement(y, "padding-left", true).replace("px", "") * 1 +
+                    getStyleElement(y, "padding-right", true).replace("px", "") * 1;
+
+            y.setAttribute("style", "width:" + s + "px;margin-left:" + ((m === w ? m : m * 0.95) - s - p) + "px;");
         }
     }
 }
@@ -290,7 +250,7 @@ function initCodes() {
 function displayControls() {
     var x = getPresentSlide();
     if (isDataSetOn(x, "data-controls-position")) {
-        switch (getValueOfData(x, "data-controls-position")) {
+        switch(x.getAttribute("data-controls-position")) {
         case "top-right":
             document.querySelector(".reveal .controls").setAttribute("class", "controls tr");
             break;
@@ -309,6 +269,56 @@ function displayControls() {
 }
 
 /*********************************************
+ * LIGHT
+ *********************************************/
+
+function lightOnOff() {
+    var x = document.querySelector("#light");
+    if(isDataSetOn(x, "data-light-off")) {
+        x.removeAttribute("data-light-off");
+        x.setAttribute("data-light-on", "");
+        document.querySelector(".reveal").classList.remove("fadeBackgroundOn");
+        document.querySelector(".reveal").classList.add("fadeBackgroundOff");
+    } else {
+        x.removeAttribute("data-light-on");
+        x.setAttribute("data-light-off", "");
+        document.querySelector(".reveal").classList.remove("fadeBackgroundOff");
+        document.querySelector(".reveal").classList.add("fadeBackgroundOn");
+    }
+}
+
+/*********************************************
+ * PDF
+ *********************************************/
+
+function initPDF() {
+    var x = document.querySelectorAll("[data-pdf]");
+    for(var i = 0, len = x.length; i < len; ++i) {
+        x[i].setAttribute("class", "pdf");
+        var y = document.createElement("iframe");
+        y.setAttribute("class", "pdf");
+        y.setAttribute("src", x[i].getAttribute("data-pdf"));
+        x[i].removeAttribute("data-pdf");
+
+        if(!isDataSetOn(x[i], "data-no-light-button")) {
+            var z = document.createElement("button");
+            var d = "yellow";
+            var c = x[i].getAttribute("data-light-color");
+            z.setAttribute("type", "button");
+            z.setAttribute("id", "light");
+            z.setAttribute("class", "button " + (c !== null ? (c !== "" ? c : d) : d));
+            z.setAttribute("onclick", "lightOnOff();");
+            z.innerText = "Turn the light off";
+            x[i].removeAttribute("data-light-color");
+            x[i].appendChild(z);
+        } else x[i].setAttribute("style", "margin-top:0px;");
+
+        x[i].removeAttribute("data-no-light-button");
+        x[i].appendChild(y);
+    }
+}
+
+/*********************************************
  * INITIALISATION
  *********************************************/
 
@@ -317,26 +327,10 @@ function initDiap() {
     initSlideNumber();
     initImages();
     initCodes();
+    initPDF();
 }
 
 function actualizeElements(event) {
     displaySlideNumber(event);
     displayControls();
-}
-
-/*********************************************
- * LIGHT
- *********************************************/
-
-function lightOnOff() {
-    var x = document.querySelector(".light");
-    if(x.innerText === "Turn the light off") {
-        x.innerText = "Turn the light on";
-        document.querySelector(".reveal").classList.remove("fadeBackgroundOff");
-        document.querySelector(".reveal").classList.add("fadeBackgroundOn");
-    } else {
-        x.innerText = "Turn the light off";
-        document.querySelector(".reveal").classList.remove("fadeBackgroundOn");
-        document.querySelector(".reveal").classList.add("fadeBackgroundOff");
-    }
 }
